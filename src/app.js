9e77d9181bc7ed9e42fd64e47c63f48efea3dd27
Yuvinska Medrano Sandoval <168204816+yuvinska@users.noreply.github.com>
2024-06-17 19:05:28 +0000

    Generador de excusas - Yuvinska

--- src/app.js
+++ src/app.js
@@ -9,3 +9,24 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+const generateExcuse = () => {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising"
+  ];
+
+  let excuse = `${who[Math.floor(Math.random() * who.length)]} ${
+    action[Math.floor(Math.random() * action.length)]
+  } ${what[Math.floor(Math.random() * what.length)]} ${
+    when[Math.floor(Math.random() * when.length)]
+  }.`;
+
+  return excuse;
+};
+
+const excuseElement = document.getElementById("excuse");
+excuseElement.textContent = generateExcuse();
